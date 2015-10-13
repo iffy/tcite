37bb9a16a3ae5428f75feb97717b618780c9d076
Matt Haggard <haggardii@gmail.com>
2015-10-13 09:33:46 -0600

    Add travis and tox config

---
+++ setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# Copyright (c) Matt Haggard
+# See LICENSE for details.
+
+
+from distutils.core import setup
+
+setup(
+    name='tcite',
+    version='0.1.0',
+    description='Python library for using tcite notation',
+    author='Matt Haggard',
+    author_email='haggardii@gmail.com',
+    url='https://github.com/iffy/tcite',
+    packages=[
+        'tcite', 'tcite.test',
+    ],
+)
